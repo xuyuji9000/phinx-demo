309fc7844c41aba9dcff6dfde9b6d8df4fa1112d
Karl Xu <karl.yogi.xu@gmail.com>
2017-08-12 20:33:40 +0800

    run first migration

--- db/migrations/20170812094054_my_new_migration.php
+++ db/migrations/20170812094054_my_new_migration.php
@@ -27,6 +27,10 @@ class MyNewMigration extends AbstractMigration
      */
     public function change()
     {
+        $table = $this->table('user_logins');
+        $table->addColumn('user_id', 'integer')
+              ->addColumn('created', 'datetime')
+              ->create();
 
     }
 }
